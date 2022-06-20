ec23e74308fcff58df03aaea2b52d7d8bd34df65
Vinicius Assis <vinicius.coassis@gmail.com>
2022-06-20 00:01:53 -0300

    Jogo Finalizado

--- scripts.js
+++ scripts.js
@@ -39,26 +39,23 @@ for (let i = 0; i< numCartas; i++){
 
 function adicionarCartas(i){
     ul.innerHTML += `<li class="cartaPadrao" onclick="select(this)"> 
-    <img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/front.png">
+    <img class="papagaio" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/front.png">
 
     <div class="cartaConteudo some">
     <img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/${arrayJogo[i]}">
-    <div class="id none"> ${arrayJogo[i]} </div>
+    <div class="id none"> ${arrayJogo[i]} </div> <span class="none">${i}</span>
     </div>
     </li>
 `; 
 }
 
-let primeiroSelecionado, segundoSelecionado, elementoSelecionado1, elementoSelecionado2, cartaSelecionada;
+let primeiroSelecionado, segundoSelecionado, cartaSelecionada, id1,id2;
 let selecionado =0, acertos=0;
 
-function select(cartaSelecionada){
 
-    // se nenhum estiver selecionado, fazer nada
-    // se primeiro a ser selecionado, avisar que foi selecionado
-    // se tiver algum selecionado, comparar
-    // se for igual, deixar
-    // se for diferente, voltar
+function select(cartaSelecionada){
+    
+    
 
     let papagaio = cartaSelecionada.querySelector("img");
     let gif = cartaSelecionada.querySelector(".cartaConteudo");
@@ -71,62 +68,77 @@ function select(cartaSelecionada){
    
     if (cartaSelecionada.querySelector(".acertou")){
         return;
-    }
+      } 
+  
 
     //quando nao tem nenhum selecionado
     if (selecionado == 0){
         selecionado = 1;
         
         primeiroSelecionado = cartaSelecionada.querySelector(".id");
+        id1 = cartaSelecionada.querySelector("span").innerHTML;
+        
     }
     // quando ja tem um selecionado 
     else if (selecionado == 1){
-        selecionado = 0;
-        segundoSelecionado = cartaSelecionada.querySelector(".id");
+    
+       selecionado = 0;
 
-        if (primeiroSelecionado.innerHTML == segundoSelecionado.innerHTML){
-            acertos++;
-            primeiroSelecionado.parentNode.classList.add("acertou");
-            segundoSelecionado.parentNode.classList.add("acertou");
-            console.log("boa!");
+        segundoSelecionado = cartaSelecionada.querySelector(".id");
+        segundoSelecionado.classList.add("block");
+        id2 = cartaSelecionada.querySelector("span").innerHTML;
+
+        // evitar clique na mesma carta 
+        if (id1 != id2){
+            if (primeiroSelecionado.innerHTML == segundoSelecionado.innerHTML){
+                acertos++;
+                primeiroSelecionado.parentNode.classList.add("acertou");
+                segundoSelecionado.parentNode.classList.add("acertou");
+                selecionado=0;
             
+                
+                  } else {
+                    setTimeout(desvirarCartas,1500);
+                    setTimeout(limpar,1501);
+                    
+                  }
+        } else {alert("aí não vale....");
       
-            
-              } else {
-                setTimeout(desvirarCartas,2000);
-                //   primeiroSelecionado="";
-                //   segundoSelecionado="";
-                  console.log("errou");
-              }
+        primeiroSelecionado = cartaSelecionada.querySelector(".id");}
     }  
         
     // conferir se venceu
         if (acertos==numCartas/2){  
-            alert("karai borracha");
-        } else{
-            console.log(`acertos depois interacoes = ${acertos}`);
+            setTimeout(alertaGanhou,1000);
         }
-}
+    }
+
+
 
 function desvirarCartas(){
-    console.log(primeiroSelecionado, segundoSelecionado);
 
     // desvirando
     primeiroSelecionado.parentNode.parentNode.classList.remove("virada");
     segundoSelecionado.parentNode.parentNode.classList.remove("virada");
 
     // aparece papagaios
-    primeiroSelecionado.parentElement.querySelector("img").classList.remove("some");
-    segundoSelecionado.parentElement.querySelector("img").classList.remove("some");
+    primeiroSelecionado.parentNode.parentNode.querySelector("img").classList.remove("some");
+    segundoSelecionado.parentNode.parentNode.querySelector("img").classList.remove("some");
     
     // some gifs
-    primeiroSelecionado.parentElement.classList.add("some");
-    segundoSelecionado.parentElement.classList.add("some");
+    primeiroSelecionado.parentNode.classList.add("some");
+    segundoSelecionado.parentNode.classList.add("some");
 
+    segundoSelecionado.classList.remove("block");
 }
 
 function alertaGanhou(){
     alert("parabens karaio");
+}
+
+function limpar(){
+    primeiroSelecionado="";
+    segundoSelecionado="";
 }
 
 function comparador() { 
